9e2627d1753c82a5bf273b591643b70fc1a66011
topdev <topdeveloper73@gmail.com>
2021-10-23 21:31:17 +0900

    Create React App Starter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React.js + Ethers.js + HardHat</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
